f6900b562fc0df196fec3e71813d6a3e5fed7416
USER <afsanaaktermim2792001@gmail.com>
2024-12-08 20:56:04 +0600

    uyyu

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late GoogleMapController googleMapController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,25 +47,63 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             draggable: true,
           ),
-        },//
+        }, //
         circles: <Circle>{
-           Circle(
-            circleId: CircleId('dengue-circle'),
-            fillColor: Colors.red.withOpacity(0.3),
-            center: LatLng(23.757021981157543, 90.38991156130523),
-            radius: 300,
-             strokeWidth: 1,
-             strokeColor: Colors.black
-          ),
           Circle(
-              circleId: CircleId('blue-circle'),
+              circleId: const CircleId('dengue-circle'),
               fillColor: Colors.red.withOpacity(0.3),
-              center: LatLng(23.757021981157543, 90.38991156130523),
+              center: const LatLng(23.757021981157543, 90.38991156130523),
               radius: 300,
               strokeWidth: 1,
-              strokeColor: Colors.black
-          ),
+              strokeColor: Colors.black),
+          Circle(
+              circleId: const CircleId('blue-circle'),
+              fillColor: Colors.red.withOpacity(0.3),
+              center: const LatLng(23.757021981157543, 90.38991156130523),
+              radius: 300,
+              strokeWidth: 1,
+              strokeColor: Colors.black,
+              visible: true,
+              onTap: () {
+                print('Entered into dangue zone');
+              }),
         },
+        polylines: <Polyline>{
+          const Polyline(
+              polylineId: PolylineId('random'),
+              color: Colors.amber,
+              width: 4,
+              jointType: JointType.round,
+              points: <LatLng>[
+                LatLng(23.762324580195234, 90.38996066898108),
+                LatLng(23.75992281933135, 90.39168935269117),
+                LatLng(23.760035129566162, 90.39367586374283),
+                LatLng(23.761606235258594, 90.39191029965878),
+              ])
+        },
+        polygons: <Polygon>{
+          Polygon(
+              polygonId: const PolygonId('poly_id'),
+              fillColor: Colors.pink.withOpacity(0.4),
+              strokeWidth: 2,
+              points: const <LatLng>[
+                LatLng(23.762324580195234, 90.38996066898108),
+                LatLng(23.75992281933135, 90.39168935269117),
+                LatLng(23.760035129566162, 90.39367586374283),
+                LatLng(23.761606235258594, 90.39191029965878),
+              ])
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          googleMapController.animateCamera(
+              CameraUpdate.newCameraPosition(const CameraPosition(
+            zoom: 17,
+            target: LatLng(23.757021981157543, 90.38991156130523),
+          )));
+        },
+        child: const Icon(Icons.location_history),
       ),
     );
   }
